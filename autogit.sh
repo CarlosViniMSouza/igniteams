5f8231daad514a199534f3f424dce57a3130340b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-25 22:42:51 -0300

    @types/styled-component in DevDependencies

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,4 @@
 git add .
-git commit -m "project inserted"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/igniteams.git
+git commit -m "@types/styled-component in DevDependencies"
 git push -u origin main
 git status
